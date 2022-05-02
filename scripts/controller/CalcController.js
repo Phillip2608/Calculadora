5015d9c592d8f038243ebcb17c02a0e568eeddbc
Phillip2608 <luizlindo2416@gmail.com>
2022-05-02 18:51:32 -0300

    foi adicionado pequenos eventos como o de apagar tudo e tambem o de erro

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,5 +1,6 @@
 class CalcController {
     constructor() {
+        this._operation = [];
         this._locale = "pt-BR";
         this._displayCalcEl = document.querySelector("#display");
         this._timeEl = document.querySelector("#hora");
@@ -26,12 +27,73 @@ class CalcController {
         });
     }
 
+    addOperation(value){
+        this._operation.push(value);
+    }
+
+    clearAll(){
+        this._operation = [];
+    }
+
+    clearEntry(){
+        this._operation.pop();
+    }
+
+    setError(){
+        this.displayCalc = "Error";
+    }
+
+    execBtn(value){
+        switch(value){
+            case 'ac':
+                this.clearAll();
+            break;
+            case 'ce':
+                this.clearEntry();
+            break;
+            case 'soma':
+                this.soma();
+            break;
+            case 'subtracao':
+                this.clearAll();
+            break;
+            case 'divisao':
+                this.clearAll();
+            break;
+            case 'multiplicacao':
+                this.clearAll();
+            break;
+            case 'porcento':
+                this.clearAll();
+            break;
+            case 'igual':
+                this.clearAll();
+            break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+            default:
+                this.setError();
+            break;
+        }
+    }
+
     initializeButtonsEvents(){
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
         buttons.forEach((btn, index)=>{
             this.addEventListenerAll(btn,'click drag', e=>{
-                console.log(btn.className.baseVal.replace("btn-", ""));
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+                this.execBtn(textBtn);
             });
 
             this.addEventListenerAll(btn ,'mouseover mouseup mousedown', e=>{
