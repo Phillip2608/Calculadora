1dd454295c44526e18ce879b3f65905163f379e0
Phillip2608 <luizlindo2416@gmail.com>
2022-05-04 15:08:58 -0300

    Foi adicionado os sons dos botoes da calculadora

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,5 +1,7 @@
 class CalcController {
     constructor() {
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
         this._operation = [];
@@ -24,6 +26,27 @@ class CalcController {
 
         this.setLastNumberToDisplay();
         this.pasteFromClipboard();
+
+        document.querySelectorAll('.btn-ac').forEach(btn=>{
+            btn.addEventListener('dblclick', e=>{
+                this.toggleAudio();
+            });
+        });
+    }
+
+    toggleAudio(){
+
+        this._audioOnOff = !this._audioOnOff;
+
+    }
+
+    playAudio(){
+        if(this._audioOnOff){
+
+            this._audio.currentTime = 0;
+
+            this._audio.play();
+        }
     }
 
     pasteFromClipboard(){
@@ -53,6 +76,8 @@ class CalcController {
     initKeyboard(){
 
         document.addEventListener('keyup', e=>{
+
+            this.playAudio();
             console.log(e);
 
             switch (e.key) {
@@ -249,6 +274,9 @@ class CalcController {
     }
 
     execBtn(value) {
+
+        this.playAudio();
+
         switch (value) {
             case 'ac':
                 this.clearAll();
